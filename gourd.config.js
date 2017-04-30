db24ff2134de4ed09fd7a8cf3d0d3d5f413b626c
fedeoo <xt_devil@sina.com>
2017-04-30 09:46:44 +0800

    Simplify gourd.config.js for hiding  webpack detail

--- gourd.config.js
+++ gourd.config.js
@@ -1,11 +1,4 @@
 
 module.exports = {
-  entry: {
-    app: ['react-hot-loader/patch', './app.js'],
-  },
-  htmlChunks: [{
-    chunks: ['app'],
-    filename: 'index.html',
-    template: './index.html',
-  }],
+  entry: './app.js',
 };
